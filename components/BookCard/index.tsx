6fee65778d8d79b67736e67fa3c8201ff3ee7a00
fredojbg <alfredo@frncubo.com>
2021-07-14 08:13:00 -0300

    fix: mock image

--- components/BookCard/index.tsx
+++ components/BookCard/index.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { View } from 'react-native';
 
 const imgBookCover = require('../../assets/images/bookcover.png');
+const imgBookCover1 = require('../../assets/images/bookcover1.png');
+const imgBookCover2 = require('../../assets/images/bookcover2.png');
 
 import { BookContainer, Book, Image, Title, Author } from './styles';
 
 const dataMock = [
-  { id: 1, cover: imgBookCover, title: 'The One Thing', author: 'Gary Keller' },
-  { id: 2, cover: imgBookCover, title: 'The One Thing', author: 'Gary Keller' },
+  { id: 1, cover: imgBookCover2, title: 'The One Thing', author: 'Gary Keller' },
+  { id: 2, cover: imgBookCover1, title: 'The One Thing', author: 'Gary Keller' },
   { id: 3, cover: imgBookCover, title: 'The One Thing', author: 'Gary Keller' },
-  { id: 4, cover: imgBookCover, title: 'The One Thing', author: 'Gary Keller' },
-  { id: 5, cover: imgBookCover, title: 'The One Thing', author: 'Gary Keller' },
+  { id: 4, cover: imgBookCover2, title: 'The One Thing', author: 'Gary Keller' },
+  { id: 5, cover: imgBookCover1, title: 'The One Thing', author: 'Gary Keller' },
   { id: 6, cover: imgBookCover, title: 'The One Thing', author: 'Gary Keller' },
-  { id: 7, cover: imgBookCover, title: 'The One Thing', author: 'Gary Keller' },
-  { id: 8, cover: imgBookCover, title: 'The One Thing', author: 'Gary Keller' },
+  { id: 7, cover: imgBookCover2, title: 'The One Thing', author: 'Gary Keller' },
+  { id: 8, cover: imgBookCover1, title: 'The One Thing', author: 'Gary Keller' },
   { id: 9, cover: imgBookCover, title: 'The One Thing', author: 'Gary Keller' },
 ]
 
