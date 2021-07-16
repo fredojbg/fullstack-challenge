bf1458f1e8e955bfac74cda73f2ce252c759c3e9
fredojbg <alfredo@frncubo.com>
2021-07-15 23:17:32 -0300

    feat: add config file && and mongo db cluster

---
+++ api/src/config/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const ENV = process.env;
+
+const graphqlPort = ENV.PORT || 9000;
+
+
+const dbUrl = `mongodb+srv://alfredoteste:alfredoteste@timelinecluster.d2aga.mongodb.net/demo`;
+
+module.exports = { jwtSecret, graphqlPort, dbUrl };
