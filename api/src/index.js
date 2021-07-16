9b77c1c250bb4d5018f893e8aa3badd296aac4e4
fredojbg <alfredo@frncubo.com>
2021-07-15 23:14:59 -0300

    feat: add setup api server

---
+++ api/src/index.js
@@ -0,0 +1,23 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const cors = require("@koa/cors");
+const bodyParser = require("koa-bodyparser");
+
+
+const app = new Koa();
+const router = new Router();
+
+
+app.use(bodyParser());
+app.use(cors());
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(500, () => {
+  console.log("##########################################################");
+  console.log("#####               STARTING SERVER                  #####");
+  console.log("##########################################################\n");
+  console.log(`App running and listening on port ${5000}...`);
+  console.log(
+    `GraphQL Server is now running on http://localhost:${5000}/graphql`
+  );
+});
